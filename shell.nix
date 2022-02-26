beacaf9bf6adef2db03238c8a51e766ece2c9bf3
Jonas Chevalier <zimbatm@zimbatm.com>
2022-02-26 17:05:41 +0100

    introduce devshell (#3)
    * introduce devshell
    
    Showcase how devshell can replace nixpkgs.mkShell.
    
     * It's compatible with nix-shell.
     * You can also use the `$(nix-build shell.nix)/entrypoint` script to
       enter the subshell for CI use-cases. It can also take a `--pure`
       argument.
     * It keeps the environment variables pretty lean compared to
       nixpkgs.mkShell.
     * It's extensible using the Nix module system.
    
    * example: add the go language to the environment

--- shell.nix
+++ shell.nix
@@ -4,6 +4,12 @@ let
 
   nixpkgs = import sources.nixpkgs { inherit system; };
 
+  devshell = import sources.devshell {
+    inherit nixpkgs;
+    inputs = null;
+    system = null;
+  };
+
   a8-scripts = import sources.a8-scripts { inherit system nixpkgs; };
 
   my-java = nixpkgs.openjdk11_headless;
@@ -13,8 +19,13 @@ let
 
   my-sbt = nixpkgs.sbt.override { jre = my-java; };
 in
-nixpkgs.mkShell {
-  buildInputs = [
+devshell.mkShell ({ extraModulesPath, ... }: {
+  imports = [
+    "${extraModulesPath}/language/go.nix"
+  ];
+
+  # Load packages
+  packages = [
     a8-scripts.a8-scripts
     my-ammonite
     my-java
@@ -22,6 +33,19 @@ nixpkgs.mkShell {
     my-scala
     nixpkgs.python3
     nixpkgs.exa
-    nixpkgs.niv
   ];
-}
+
+  # Set env vars
+  env = [
+    { name = "KEY"; value = "1"; }
+    { name = "SDFSDFG"; eval = "$KEY-xxx"; }
+  ];
+
+  # Add commands in the menu
+  commands = [
+    {
+      package = nixpkgs.niv;
+      category = "nix";
+    }
+  ];
+})
