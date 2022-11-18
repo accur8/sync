bcca9033d7bbff0a1a3de6995ee98991dbe4e2f0
glen@stella <glen@accur8software.com>
2022-11-18 14:27:41 -0500

    added ltrim and rtrim to StringOps

---
+++ shared/jvm/src/test/scala/a8/shared/StringOpsTest.scala
@@ -0,0 +1,54 @@
+package a8.shared
+
+
+import a8.shared.app.BootstrapConfig.TempDir
+import zio.{Task, ULayer, ZIO, ZLayer}
+import zio.stream.ZStream
+import zio.test.{TestResult, ZIOSpecDefault, assertTrue}
+import SharedImports._
+import a8.sync.ReplayableByteStreamSpec.runInParallel
+
+object StringOpsTest extends ZIOSpecDefault {
+
+  case class Test(input: String, expected: String)
+
+  def spec =
+    suite("string ops")(
+
+      test("ltrim") {
+        runTests(
+          Seq(
+            Test("  abc", "abc"),
+            Test("  abc  ", "abc  "),
+            Test("abc", "abc"),
+            Test("abc   ", "abc   "),
+          ),
+          _.ltrim
+        )
+      },
+
+      test("rtrim") {
+        runTests(
+          Seq(
+            Test("   abc", "   abc"),
+            Test("   abc  ", "   abc"),
+            Test("abc", "abc"),
+            Test("abc   ", "abc"),
+          ),
+          _.rtrim
+        )
+      },
+    )
+
+
+  def runTests(tests: Seq[Test], fn: String=>String): Task[TestResult] =
+    zsucceed(
+      tests
+        .map { test =>
+          val actual = fn(test.input)
+          assertTrue(actual == test.expected)
+        }
+        .reduce(_ && _)
+    )
+
+}
--- shared/shared/src/main/scala/a8/shared/ops/StringOps.scala
+++ shared/shared/src/main/scala/a8/shared/ops/StringOps.scala
@@ -8,13 +8,19 @@ import a8.shared.SharedImports._
 import zio.stream.{UStream, ZStream}
 
 
+object StringOps {
+  val ltrimPattern = Pattern.compile("^\\s+")
+  val rtrimPattern = Pattern.compile("\\s+$")
+}
+
 class StringOps(private val source: String) extends AnyVal {
 
   def isNotBlank = source.exists(!_.isWhitespace)
   def isBlankX = source.trim.length == 0
   def toCi = CIString(source)
   def =:=(right: String) = source.equalsIgnoreCase(right)
-  def rtrim = source.replaceAll("\\s+$", "")
+  def ltrim = StringOps.ltrimPattern.matcher(source).replaceAll("")
+  def rtrim = StringOps.rtrimPattern.matcher(source).replaceAll("")
   def toUri = Uri.unsafeParse(source)
 
   def toChunk: zio.Chunk[Byte] =
