612c2b5c32d908d5b3dd9f916a1b0a8bd2a19d91
glen@stella <glen@accur8software.com>
2022-11-30 09:55:53 -0500

    added Synchronize and associated unit tests

---
+++ shared/shared/src/main/scala/a8/shared/Synchronize.scala
@@ -0,0 +1,105 @@
+package a8.shared
+
+
+import SharedImports._
+
+object Synchronize {
+
+  sealed trait Action[A]
+  object Action {
+    case class Insert[A](value: A) extends Action[A]
+    case class Update[A](before: A, after: A) extends Action[A]
+    case class Delete[A](value: A) extends Action[A]
+    case class Noop[A](before: A, after: A) extends Action[A]
+  }
+
+  /**
+   * Analyzes the source and target and returns
+   * the Action's (Insert, Update, Delete, Noop) needed to be applied to the target for it to match the source
+   */
+  def apply[A](source: Vector[A], target: Vector[A]): Vector[Action[A]] =
+    apply(source, target, identity)
+
+  /**
+   * Analyzes the source and target and returns
+   * the Action's (Insert, Update, Delete, Noop) needed to be applied to the target for it to match the source
+   */
+  def apply[A,B](source: Vector[A], target: Vector[A], correlateBy: A=>B): Vector[Action[A]] = {
+
+    import Action._
+
+    val sourceBy = source.toMapTransform(correlateBy)
+    val targetBy = target.toMapTransform(correlateBy)
+
+    val deletesAndUpdatesAndNoops =
+      target
+        .map(t => t -> sourceBy.get(correlateBy(t)))
+        .map {
+          case (s, Some(t)) =>
+            // in both target and source
+            if ( s == t )
+              Noop(s, t)
+            else
+              Update(s, t)
+
+          case (t, None) =>
+            // in target not in source it is an insert
+            Delete(t)
+
+        }
+
+    // in source and not in target is an insert
+    val inserts =
+      source
+        .filterNot(s => targetBy.contains(correlateBy(s)))
+        .map(s => Insert(s))
+
+    deletesAndUpdatesAndNoops ++ inserts
+
+  }
+
+
+
+  sealed trait AsymetricAction[A,B]
+  object AsymetricAction {
+    case class Insert[A,B](value: A) extends AsymetricAction[A,B]
+    case class Update[A,B](before: A, after: B) extends AsymetricAction[A,B]
+    case class Delete[A,B](value: B) extends AsymetricAction[A,B]
+  }
+
+  /**
+   * Analyzes the source and target and returns
+   * the Action's (Insert, Update, Delete, Noop) needed to be applied to the target for it to match the source
+   */
+  def asymetric[A,B,C](source: Iterable[A], target: Iterable[B], correlateSourceToC: A=>C, correlateTargetToC: B=>C): Vector[AsymetricAction[A,B]] = {
+
+    import AsymetricAction._
+
+    val sourceBy = source.toMapTransform(correlateSourceToC)
+    val targetBy = target.toMapTransform(correlateTargetToC)
+
+    val deletesAndUpdatesAndNoops: Iterable[AsymetricAction[A,B]] =
+      target
+        .map(t => t -> sourceBy.get(correlateTargetToC(t)))
+        .map {
+          case (t, Some(s)) =>
+            Update[A,B](s, t)
+
+          case (t, None) =>
+            // in target not in source it is an insert
+            Delete[A,B](t)
+
+        }
+
+    // in source and not in target is an insert
+    val inserts: Iterable[Insert[A, B]] =
+      source
+        .filterNot(s => targetBy.contains(correlateSourceToC(s)))
+        .map(s => Insert[A,B](s))
+
+    (deletesAndUpdatesAndNoops ++ inserts).toVector
+
+  }
+
+
+}
---
+++ shared/shared/src/test/scala/a8/shared/SynchronizeTest.scala
@@ -0,0 +1,101 @@
+package a8.shared
+
+import org.scalatest.GivenWhenThen
+import org.scalatest.funsuite.AnyFunSuite
+
+class SynchronizeTest extends AnyFunSuite with GivenWhenThen {
+
+  case class ItemA(
+    id: Int,
+    name: String,
+  )
+
+  case class ItemB(
+    correlationId: Int,
+    description: String,
+  )
+
+  test("Synchonrize.asymetric") {
+
+    import Synchronize.AsymetricAction
+
+    val a =
+      Vector(
+        ItemA(1, "1"),
+        ItemA(2, "2"),
+      )
+
+    val b =
+      Vector(
+        ItemB(2, "2"),
+        ItemB(3, "3"),
+      )
+
+
+    def testRun(source: Iterable[ItemA], target: Iterable[ItemB], expected: Iterable[AsymetricAction[ItemA,ItemB]]): Unit = {
+      val actual =
+        Synchronize.asymetric[ItemA,ItemB,Int](
+          source,
+          target,
+          _.id,
+          _.correlationId
+        )
+
+      assert(actual == expected.toVector)
+
+    }
+
+    import AsymetricAction._
+    testRun(
+      a,
+      b,
+      Vector(
+        Update(ItemA(2,"2"),ItemB(2,"2")),
+        Delete(ItemB(3,"3")),
+        Insert(ItemA(1,"1")),
+      )
+    )
+
+  }
+
+  test("Synchonrize.apply") {
+
+    val a =
+      Vector(
+        ItemA(1, "1"),
+        ItemA(2, "2"),
+      )
+
+    val b =
+      Vector(
+        ItemA(2, "2"),
+        ItemA(3, "3"),
+      )
+
+
+    import Synchronize.Action
+    import Action._
+    def testRun(source: Vector[ItemA], target: Vector[ItemA], expected: Vector[Action[ItemA]]): Unit = {
+      val actual =
+        Synchronize[ItemA,Int](
+          source,
+          target,
+          _.id,
+        )
+
+      assert(actual == expected)
+
+    }
+
+    testRun(
+      a,
+      b,
+      Vector(
+        Noop(ItemA(2,"2"),ItemA(2,"2")),
+        Delete(ItemA(3,"3")),
+        Insert(ItemA(1,"1")),
+      )
+    )
+
+  }
+}
