2cb53b722def7b2b3a97caefd725b2f0471f3f29
Raphael Enns <raphael.enns@gmail.com>
2023-02-14 12:08:19 -0600

    Fix compile warning in A8LogFormatter

--- shared/shared/src/main/scala/a8/shared/app/A8LogFormatter.scala
+++ shared/shared/src/main/scala/a8/shared/app/A8LogFormatter.scala
@@ -28,16 +28,11 @@ trait A8LogFormatter extends LogFormatter {
   protected val timestampFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss.SSS")
   protected val levelWidth = 14
 
-  object impl {
-    // bypass the deprecation warning
-    object Outer { @deprecated("", "") class Inner { def getThreadID(r: LogRecord) = r.getThreadID }; object Inner extends Inner }
-  }
-
   override def formatLog(r: LogRecord): String = {
     val dateTime = ZonedDateTime.ofInstant(Instant.ofEpochMilli(r.getMillis), LogTimestampFormatter.systemZone)
     val timestamp = withColor(Console.BLUE, timestampFormatter.format(dateTime))
     val level = highlightLog(r.level, r.level.name.toUpperCase).padTo(levelWidth, ' ')
-    val thread = withColor(Console.WHITE, impl.Outer.Inner.getThreadID(r).toString)
+    val thread = withColor(Console.WHITE, r.getThreadID.toString)
     val loggerName = withColor(Console.WHITE, r.getLoggerName)
     val message = highlightLog(r.level, r.getMessage)
     val location = r.source.map(source => s" ${withColor(Console.BLUE, s"- (${source.fileLoc})")}").getOrElse("")
