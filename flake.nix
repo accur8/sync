3f0663e525a73285199afd16589135516f96ea16
glen@starbak <glen@accur8software.com>
2025-09-29 10:15:50 -0400

    more nix-pins enhancements

--- flake.nix
+++ flake.nix
@@ -1,24 +1,15 @@
 {
-  description = "Project using centralized nix-pins";
-
+  
   inputs = {
     nix-pins.url = "git+ssh://git@git.accur8.net/a8/nix-pins";
-    nixpkgs.follows = "nix-pins/nixpkgs";
-    devshell.follows = "nix-pins/devshell";
   };
 
-  outputs = { self, nix-pins, nixpkgs, devshell }:
-  {
-    devShells = nix-pins.lib.forEachSystem (system:
-      let
-        pkgs = nix-pins.pkgsFor system;
-        # Add any project-specific packages or overrides here
-      in {
-        default = nix-pins.lib.shells.scala3 {
-          inherit pkgs;
-          # Add project-specific packages here:
-          # extraInputs = [ pkgs.postgresql ];
-        };
-      });
-  };
+  outputs = { self, nix-pins }:
+    {
+      devShells = nix-pins.lib.forEachSystem (system:
+        {
+          default = nix-pins.devShells.${system}.scala3;
+        }
+      );
+    };
 }
