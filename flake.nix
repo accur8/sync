13abe523ce0736bb6f220ba119cf0259e5609c89
glen@starbak <glen@accur8software.com>
2025-10-04 09:30:57 -0400

    Switch to composable devShells and add AI coding assistants

--- flake.nix
+++ flake.nix
@@ -1,15 +1,16 @@
 {
-  
+
   inputs = {
     nix-pins.url = "git+ssh://git@git.accur8.net/a8/nix-pins";
   };
 
-  outputs = { self, nix-pins }:
+  outputs = { self, nix-pins, ... }:
     {
-      devShells = nix-pins.lib.forEachSystem (system:
-        {
-          default = nix-pins.devShells.${system}.scala3;
-        }
-      );
+      devShells = nix-pins.lib.composeDevShells {
+        default = {pkgs, frags, ...}: [
+          (frags.scala_3 { })
+          (frags.aiCodingAssistants { })
+        ];
+      };
     };
 }
